15bbf7ea28845cf264f0a215fcce045744f80bc8
Damian <damianszkudlarek@gmail.com>
2017-08-27 14:35:53 +0200

    + Balanced Tree + Tree height + Find in tree

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,8 +1,92 @@
 public class BinaryTree {
 
     public static void main(String[] args) {
-        TreeElement randomBinaryTree = generateRandomBinaryTree(6, 0.5, 99);
+        TreeElement randomBinaryTree = generateRandomBinaryTree(6, 0.5, 10);
         System.out.println(randomBinaryTree.toString());
+
+        System.out.println(findInTree(5, randomBinaryTree));
+        System.out.println(getTreeHeight(randomBinaryTree));
+    }
+
+    public static TreeElement generateBalancedTree(int elementsNumber, int range)
+    {
+        TreeElement treeElement = new TreeElement();
+
+        int leftTreeSize = 0;
+        int rightTreeSize = 0;
+        if (elementsNumber > 0) {
+            leftTreeSize = elementsNumber/2;
+            rightTreeSize = elementsNumber - leftTreeSize;
+
+            int randomValue = (int) (Math.random()*range);
+
+            treeElement.setElementValue(randomValue);
+            treeElement.setLeftElement(generateBalancedTree(leftTreeSize, range));
+            treeElement.setRightElement(generateBalancedTree(rightTreeSize, range));
+
+        }
+        return treeElement;
+    }
+
+    public static int getTreeHeight(TreeElement root)
+    {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftTreeHeight = getTreeHeight(root.getLeftElement());
+            int rightTreeHeight = getTreeHeight(root.getRightElement());
+            if (leftTreeHeight > rightTreeHeight) {
+                return leftTreeHeight+1;
+            } else {
+                return rightTreeHeight+1;
+            }
+        }
+
+    }
+
+
+    public static boolean findInTree(int searchedValue, TreeElement root)
+    {
+        boolean founded = false;
+        if (root != null) {
+            if(searchedValue == root.getElementValue()) {
+                founded = true;
+            } else {
+                founded = findInTree(searchedValue, root.getLeftElement());
+                if(!founded) {
+                    founded = findInTree(searchedValue, root.getRightElement());
+                }
+            }
+        }
+
+        return founded;
+    }
+
+    public static void preorder(TreeElement root)
+    {
+        if (root != null) {
+            System.out.println(root.getElementValue());
+            preorder(root.getLeftElement());
+            preorder(root.getRightElement());
+        }
+    }
+
+    public static void inorder(TreeElement root)
+    {
+        if (root != null) {
+            inorder(root.getLeftElement());
+            System.out.println(root.getElementValue());
+            inorder(root.getRightElement());
+        }
+    }
+
+    public static void postorder(TreeElement root)
+    {
+        if (root != null) {
+            postorder(root.getLeftElement());
+            postorder(root.getRightElement());
+            System.out.println(root.getElementValue());
+        }
     }
 
     public static TreeElement generateRandomBinaryTree(int elementsNumber, double probability, int range) {
